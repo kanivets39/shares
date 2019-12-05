2d088abe34b953562f0e00f5af18945f0ecd0cd5
kanivets39 <47155060+kanivets39@users.noreply.github.com>
2019-12-05 15:53:49 +0200

    Initial commit

---
+++ src/main/java/com/kanivets/shares/SharesApplication.java
@@ -0,0 +1,13 @@
+package com.kanivets.shares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kanivets/shares/SharesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanivets.shares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
